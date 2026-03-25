7a286e4216aa6d7a89ef7d16b717ade37c3e03a1
Eduardo Hansel <eduardohansel10@gmail.com>
2026-03-25 15:51:47 -0300

    Chore: incluindo urls do novo app às urls reconhecidas.

--- greenUpCore/urls.py
+++ greenUpCore/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('greenup/', include('usuarios.urls')),
+    path('greenup/', include('app.urls')),
 ]
