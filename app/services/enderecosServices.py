183369608726ed19a2822e8539fac9addfe72b08
Eduardo Hansel <eduardohansel10@gmail.com>
2026-03-25 17:36:42 -0300

    Feat: implementação do service de listagem específica de endereço.

--- app/services/enderecosServices.py
+++ app/services/enderecosServices.py
@@ -3,3 +3,10 @@ from app.models.enderecosModel import Endereco
 def criar_endereco_service(data):
     endereco = Endereco.objects.create(**data)
     return endereco
+
+def listar_endereco_especifico_service(pk):
+    try:
+        endereco = Endereco.objects.get(pk=pk)
+        return endereco
+    except Endereco.DoesNotExist:
+        return None
