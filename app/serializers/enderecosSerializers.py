01b502538af629e9c73e9e8db454cbf4b6f0ac68
Eduardo Hansel <eduardohansel10@gmail.com>
2026-03-25 15:54:39 -0300

    Feat: implementação do serializer de resposta e de criação de endereço.

---
+++ app/serializers/enderecosSerializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from app.models.enderecosModel import Endereco
+
+class CriarEnderecoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Endereco
+        fields = '__all__'
+
+class EnderecoResponseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Endereco
+        fields = '__all__'
