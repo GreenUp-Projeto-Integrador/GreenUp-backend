f5d70dd07332af50acd39b74e5858e62c6264cd6
Eduardo Hansel <eduardohansel10@gmail.com>
2026-03-25 22:21:12 -0300

    Chore: implementação do service de atualização de endereço.

--- app/services/enderecosServices.py
+++ app/services/enderecosServices.py
@@ -10,3 +10,15 @@ def listar_endereco_especifico_service(pk):
         return endereco
     except Endereco.DoesNotExist:
         return None
+
+def update_endereco_service(data, pk):
+    try:
+        endereco_obj = Endereco.objects.get(pk=pk)
+    except Endereco.DoesNotExist:
+        return None
+
+    for key, value in data.items():
+        setattr(endereco_obj, key, value)
+
+    endereco_obj.save(update_fields=data.keys())
+    return endereco_obj
