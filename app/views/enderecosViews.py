1197f256340901ce4bf8def6da39c7f1d03ca47f
Eduardo Hansel <eduardohansel10@gmail.com>
2026-03-25 22:20:37 -0300

    Chore: implementação da view de atualização de endereço.

--- app/views/enderecosViews.py
+++ app/views/enderecosViews.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from app.serializers.enderecosSerializers import CriarEnderecoSerializer, EnderecoResponseSerializer, ListarEnderecoEspecificoSerializer
-from app.services.enderecosServices import criar_endereco_service, listar_endereco_especifico_service
+from app.serializers.enderecosSerializers import CriarEnderecoSerializer, EnderecoResponseSerializer, ListarEnderecoEspecificoSerializer, UpdateEnderecoSerializer
+from app.services.enderecosServices import criar_endereco_service, listar_endereco_especifico_service, update_endereco_service
 
 class CriarEnderecoView(APIView):
 
@@ -54,6 +54,43 @@ class ListarEnderecoEspecificoView(APIView):
                 "message": "Endereço encontrado com sucesso",
                 "endereco": endereco_serializer.data,
             })
+
+        except Exception as e:
+            return Response({
+                "success": False,
+                "message": "Um erro inesperado ocorreu",
+                "errors": str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class UpdateEnderecoView(APIView):
+
+    @staticmethod
+    def patch(request, pk):
+        try:
+            serializer = UpdateEnderecoSerializer(data=request.data, partial=True)
+
+            if not serializer.is_valid():
+                return Response({
+                    "success": False,
+                    "message": "Erro ao atualizar endereço",
+                    "errors": serializer.errors
+                }, status=status.HTTP_400_BAD_REQUEST)
+
+            endereco = update_endereco_service(serializer.validated_data, pk)
+
+            if not endereco:
+                return Response({
+                    "success": False,
+                    "message": "Endereço não encontrado"
+                }, status=status.HTTP_404_NOT_FOUND)
+
+            endereco_serializer = EnderecoResponseSerializer(endereco)
+
+            return Response({
+                "success": True,
+                "message": "Endereço atualizado com sucesso",
+                "endereco": endereco_serializer.data,
+            }, status=status.HTTP_200_OK)
 
         except Exception as e:
             return Response({
