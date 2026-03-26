139bdbe1ff794917c2ed3d7d5ee56604f3f52e4a
Eduardo Hansel <eduardohansel10@gmail.com>
2026-03-25 21:47:15 -0300

    Chore: adição da view de atualização de endereço ao init.

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,3 +1,4 @@
 # Views de endereço
 from app.views.enderecosViews import CriarEnderecoView
 from app.views.enderecosViews import ListarEnderecoEspecificoView
+from app.views.enderecosViews import UpdateEnderecoView
