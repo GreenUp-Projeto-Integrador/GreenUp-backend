504c8701f0eff5e469c73feba82d0b3ebeddb2a1
Eduardo Hansel <eduardohansel10@gmail.com>
2026-03-25 22:23:56 -0300

    Chore: implementação do serializer de atualização de endereço.

--- app/serializers/enderecosSerializers.py
+++ app/serializers/enderecosSerializers.py
@@ -13,6 +13,28 @@ class ListarEnderecoEspecificoSerializer(serializers.ModelSerializer):
         model = Endereco
         fields = '__all__'
 
+class UpdateEnderecoSerializer(serializers.ModelSerializer):
+    cep = serializers.CharField(required=False)
+    cidade = serializers.CharField(required=False)
+    estado = serializers.CharField(required=False)
+    rua = serializers.CharField(required=False)
+    numero = serializers.CharField(required=False)
+    complemento = serializers.CharField(required=False)
+
+    def validate(self, attrs):
+        campos_validos = set(self.fields.keys())
+        campos_recebidos = set(self.initial_data.keys())
+        campos_invalidos = campos_recebidos - campos_validos
+
+        if campos_invalidos:
+            raise serializers.ValidationError(f"Campos inválidos: {campos_invalidos}")
+
+        return attrs
+
+    class Meta:
+        model = Endereco
+        fields = '__all__'
+
 class EnderecoResponseSerializer(serializers.ModelSerializer):
 
     class Meta:
